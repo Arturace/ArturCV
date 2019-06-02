75307d6dfd7cf42672169e2cd9de89cf230b5b50
Artur Madjidov <artur.madjidov98@gmail.com>
2019-06-02 00:44:20 -0300

    Disliking typescript now. Need proxies!!

--- dist/scripts/programming.js
+++ dist/scripts/programming.js
@@ -1,14 +1,33 @@
-document.getElementsByName('HaveExperience').forEach((value) => {
+Array.from(document.getElementsByClassName('have-experience')).forEach((value) => {
     Array.from(value.children).forEach(element => {
-        if (element instanceof HTMLUListElement) {
-            Array.from(element.children).forEach(listElement => {
-                listElement.addEventListener('click', function () {
-                    document.getElementById('Programming').classList.toggle('detail');
-                    if (listElement.children.item(1) != null) {
-                        document.getElementById('ProgrammingDetail').innerHTML = listElement.children.item(1).innerHTML;
-                    }
-                });
-            });
+        element.addEventListener('click', function () {
+            console.log('click');
+            document.getElementById('Programming').classList.toggle('detail');
+            if (element.children.item(1) != null) {
+                document.getElementById('ProgrammingDetail').innerHTML = element.children.item(1).innerHTML;
+            }
+        });
+    });
+    let scrollInterval = 0;
+    let scrollStart;
+    value.addEventListener('wheel', function (e) {
+        clearInterval(scrollInterval);
+        scrollStart = new Date();
+        if (e.deltaY > 0) {
+            scrollInterval = setInterval(function () {
+                value.scrollTo(value.scrollLeft + 2, 0);
+                if (scrollStart.getTime() < new Date().getTime() - 1 * 100) {
+                    clearInterval(scrollInterval);
+                }
+            }, 0.1);
+        }
+        else if (e.deltaY < 0 && value.scrollLeft > 0) {
+            scrollInterval = setInterval(function () {
+                value.scrollTo(value.scrollLeft - 2, 0);
+                if (scrollStart.getTime() < new Date().getTime() - 1 * 100) {
+                    clearInterval(scrollInterval);
+                }
+            }, 0.1);
         }
     });
 });
--- scripts/programming.ts
+++ scripts/programming.ts
@@ -1,20 +1,42 @@
-document.getElementsByName('HaveExperience').forEach((value) => {
+Array.from(document.getElementsByClassName('have-experience')).forEach((value) => {
    Array.from(value.children).forEach(element => {
-      if (element instanceof HTMLUListElement){
-         Array.from(element.children).forEach(listElement => {
-            listElement.addEventListener('click', function() {
-               //listElement.classList.toggle('active');
-               document.getElementById('Programming').classList.toggle('detail');
+      element.addEventListener('click', function() {
+         console.log('click');
+         //listElement.classList.toggle('active');
+         document.getElementById('Programming').classList.toggle('detail');
 
-               if (listElement.children.item(1) != null) {
-                  //showing the details
-                  document.getElementById('ProgrammingDetail').innerHTML = listElement.children.item(1).innerHTML;
-               }
-            });
-         });
+         if (element.children.item(1) != null) {
+            //showing the details
+            document.getElementById('ProgrammingDetail').innerHTML = element.children.item(1).innerHTML;
+         }
+      });
+   });
+
+   //Setting horizontal scroll
+   let scrollInterval: number = 0;
+   let scrollStart: Date;
+   value.addEventListener('wheel', function(e: WheelEvent) {
+      clearInterval(scrollInterval);
+      scrollStart = new Date();
+      if (e.deltaY > 0) { 
+         //going right
+         scrollInterval = setInterval(function() {
+            value.scrollTo(value.scrollLeft + 2,0);
+            if (scrollStart.getTime() < new Date().getTime() - 1 * 100 ) {
+               clearInterval(scrollInterval);
+            } 
+         }, 0.1);
+      } else if (e.deltaY < 0 && value.scrollLeft > 0) {
+         //going left
+         scrollInterval = setInterval(function() {
+            value.scrollTo(value.scrollLeft - 2 ,0);
+            if (scrollStart.getTime() < new Date().getTime() - 1 * 100 ) {
+               clearInterval(scrollInterval);
+            } 
+         }, 0.1);
       }
    });
-});
+}); 
 
 document.getElementById('CloseDetail').addEventListener('click', () => {
    document.getElementById('Programming').classList.remove('detail');
