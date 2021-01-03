19fddeceb8048017fafc29beebdc8ab4a192510c
Artur Madjidov <artur.madjidov98@gmail.com>
2021-01-03 00:44:25 -0500

    Improve deploy-watch
    + spawns the http-server, to be able to control it's execution cross-platform
    + builds & deploys at the start of the script, making it the only point of entry.

--- dev/deploy-watch.js
+++ dev/deploy-watch.js
@@ -2,36 +2,49 @@ const FS = require('fs');
 const PATH = require('path');
 const DEPLOY = require('./deploy.utils');
 const BUILD = require('./build.utils');
+const { spawn: SPAWN } = require('child_process');
 
 const BUILD_N_DEPLOY = (() => {
   let buildTimeout;
-  return () => {
+  return (isInitial = false) => {
     clearTimeout(buildTimeout);
     buildTimeout = setTimeout(() => {
-      console.log('Change detected, re-building and deploying.');
+      console.log(
+        isInitial
+          ? 'Initial build & deploy'
+          : 'Change detected, re-building & deploying.'
+      );
       BUILD.build();
       DEPLOY();
     }, 666);
   };
 })();
 
+BUILD_N_DEPLOY(true);
+
+const SUB_PROC = SPAWN('http-server', [ 'local-deploy' ], { shell: true });
+
+SUB_PROC.on('error', (err) => console.error('Failed to start subprocess.', err));
+SUB_PROC.stdout.on('data', (data) => console.log(`${data}`));
+SUB_PROC.stderr.on('data', (data) => console.error(`${data}`));
+
 // files to watch
 [
   'template.html'
   , 'dist/favicon.ico'
-].forEach(v => 
+].forEach(v =>
   FS.watch(
     PATH.join(process.cwd(), v)
-    , BUILD_N_DEPLOY)
+    , () => BUILD_N_DEPLOY())
 );
 // folders to watch
 [
   'styles/'
   , 'dist/img/'
   , 'template-data/'
-].forEach(v => 
+].forEach(v =>
   FS.watch(
     PATH.join(process.cwd(), v)
     , { recursive: true }
-    , BUILD_N_DEPLOY)
+    , () => BUILD_N_DEPLOY())
 );
