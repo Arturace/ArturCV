ed815f48c9ee742016b570e529a799800855dcd9
Artur Madjidov <artur.madjidov98@gmail.com>
2021-01-16 23:59:59 -0500

    Add ignore mecanism to deploy script
    + Ignores "_og." files.

--- dev/deploy.js
+++ dev/deploy.js
@@ -1,1 +1,16 @@
-require('./deploy.utils')();
+/*
+deploy.js DeployFolder --ignore
+*/
+let argv = require('minimist')(process.argv.slice(2));
+
+if (!argv._) throw new Error('Deploy folder missing,');
+argv.ignore = argv.ignore ? argv.ignore : argv.i;
+
+require('./deploy.utils')(
+  require('path').join(process.cwd(), argv._[0])
+  , argv.ignore 
+    ? argv.ignore instanceof Array 
+      ? argv.ignore.map(v => new RegExp(v))
+      : [ new RegExp(argv.ignore) ]
+    : []
+);
