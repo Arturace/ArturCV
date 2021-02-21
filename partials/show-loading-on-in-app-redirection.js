1e99ee6eb9decbe7d4f8f72c945d1ca4f35fc4ad
Artur Madjidov <artur.madjidov98@gmail.com>
2021-02-21 15:33:54 -0500

    Move page-loading on redirect to a partial

---
+++ partials/show-loading-on-in-app-redirection.js
@@ -0,0 +1,22 @@
+(() => {
+  const PAGE_LOADING = document.getElementsByTagName('page-loading')[0];
+  let leavePageLoadingShown = false;
+  document.body.addEventListener('click', (e) => {
+    if (leavePageLoadingShown) return;
+    const T = e.target;
+    if (T.nodeName == 'A' 
+        && location.hostname == T.hostname) {
+        e.preventDefault();
+        PAGE_LOADING
+          .show()
+          .then(() => {
+              leavePageLoadingShown = true;
+              T.dispatchEvent(new MouseEvent('click', {
+                bubbles: true,
+                cancelable: true,
+                view: window
+              }));
+          });
+    }
+  });
+})();
